4fb4dfeb75430f85374ba438ce49d70bf845b57f
qsniyg <qsniyg@mail.com>
2016-09-28 15:08:02 -0700

    Add official twitter API support

--- rssit/generators/twitter.py
+++ rssit/generators/twitter.py
@@ -6,6 +6,14 @@ import re
 import rssit.util
 import bs4
 import demjson
+import tweepy
+from calendar import timegm
+import xml.sax.saxutils
+
+try:
+    from rfc822 import parsedate
+except ImportError:
+    from email.utils import parsedate
 
 
 info = {
@@ -13,9 +21,17 @@ info = {
     "codename": "twitter",
     "config": {
         "author_username": False,
-        "with_replies": True
+        "with_replies": True,
+        "consumer_key": "",
+        "consumer_secret": "",
+        "access_token": "",
+        "access_secret": ""
     }
 }
+
+
+auths = {}
+user_infos = {}
 
 
 def get_string(element):
@@ -34,7 +50,7 @@ def check(url):
     return re.match(r"^https?://(?:\w+\.)?twitter.com/(?P<user>[^/]*)", url) != None
 
 
-def generate(config, path):
+def generate_html(user, config, path):
     match = re.match(r"^https?://(?:\w+\.)?twitter.com/(?P<user>[^/]*)", config["url"])
 
     if match == None:
@@ -141,3 +157,109 @@ def generate(config, path):
         })
 
     return feed
+
+
+def generate_api(user, config, path):
+    auth_key = config["consumer_key"] + config["consumer_secret"] +\
+               config["access_token"] + config["access_secret"]
+
+    if auth_key in auths:
+        api = auths[auth_key]["api"]
+    else:
+        auth = tweepy.OAuthHandler(config["consumer_key"], config["consumer_secret"])
+        auth.set_access_token(config["access_token"], config["access_secret"])
+
+        api = tweepy.API(auth)
+
+        auths[auth_key] = {
+            "auth": auth,
+            "api": api
+        }
+
+    if not user in user_infos:
+        user_infos[user] = api.get_user(id=user)
+
+    user_info = user_infos[user]
+
+    title = "@" + user_info.screen_name
+
+    if not config["author_username"] and "name" in user_info.__dict__ and len(user_info.name) > 0:
+        title = user_info.name
+
+    if "description" in user_info.__dict__ and len(user_info.description) > 0:
+        description = user_info.description
+    else:
+        description = "%s's twitter" % author
+
+    feed = {
+        "title": title,
+        "description": description,
+        "author": user_info.screen_name,
+        "social": True,
+        "entries": []
+    }
+
+
+    tl = api.user_timeline(id=user, count=config["count"])
+
+    if not tl:
+        return None
+
+    for obj in tl:
+        caption = xml.sax.saxutils.unescape(re.sub(" *http[^ ]*t\.co/[^ ]*", "", obj.text))
+        #date = obj.created_at.timestamp()
+        date = rssit.util.localize_datetime(datetime.datetime.fromtimestamp(timegm(parsedate(obj._json["created_at"])), None))
+
+        entrydict = {
+            "url": "https://twitter.com/statuses/" + obj.id_str,
+            "caption": caption,
+            "date": date,
+            "author": obj.author.screen_name,
+            "images": [],
+            "videos": []
+        }
+
+        #pprint.pprint(obj.__dict__)
+
+        if not "extended_entities" in obj.__dict__:
+            continue
+
+        for media in obj.__dict__["extended_entities"]["media"]:
+            if media["type"] == "photo":
+                entrydict["images"].append(media["media_url"])
+            elif media["type"] == "video" or media["type"] == "animated_gif":
+                videodict = {
+                    "image": media["media_url"]
+                }
+
+                variants = media["video_info"]["variants"]
+
+                max_bitrate = -1
+                curr = None
+                for variant in variants:
+                    if "bitrate" in variant and variant["bitrate"] > max_bitrate:
+                        curr = variant
+
+                if not curr:
+                    curr = variants[0]
+
+                videodict["video"] = curr["url"]
+                entrydict["videos"].append(videodict)
+
+        feed["entries"].append(entrydict)
+
+    return feed
+
+
+def generate(config, path):
+    match = re.match(r"^https?://(?:\w+\.)?twitter.com/(?P<user>[^/]*)", config["url"])
+
+    if match == None:
+        return None
+
+    user = match.group("user")
+
+    if len(config["consumer_key"]) > 0:
+        return generate_api(user, config, path)
+    else:
+        return generate_html(user, config, path)
